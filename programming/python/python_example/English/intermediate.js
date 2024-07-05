c00d4e7a8065c9039bb193b14f636cf536145691
01one <84787013+01one@users.noreply.github.com>
2024-07-05 18:37:50 +0600

    Update intermediate.js

--- programming/python/python_example/English/intermediate.js
+++ programming/python/python_example/English/intermediate.js
@@ -1,3 +1,44 @@
 let intermediateExamples = {
 
+  "Simple Class": `# Python class Example 
+class Bird:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+
+
+    def sing(self):
+		
+        print(self.name, 'the', self.species, 'is singing!')
+
+
+
+    def fly(self, distance):
+        print(self.name, 'flew',distance, 'meters.')
+
+
+
+    def current_age(self):
+        self.age += 1
+        print(self.name, 'You are now', self.age, 'years old.')
+
+
+
+# Create instances of the Bird class
+#name, species, age
+bird1 = Bird("Tweety", "Dove", 2)
+bird2 = Bird("Twinkle", "Parrot", 5)
+
+
+bird1.sing()
+bird1.fly(100)
+
+
+
+bird1.current_age()`
+
+
+  
 };
