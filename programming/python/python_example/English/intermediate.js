f17097de016f4c7ea3a1af3b21dbf25aeea78721
01one <84787013+01one@users.noreply.github.com>
2024-07-05 18:40:34 +0600

    Update intermediate.js

--- programming/python/python_example/English/intermediate.js
+++ programming/python/python_example/English/intermediate.js
@@ -7,23 +7,15 @@ class Bird:
         self.species = species
         self.age = age
 
-
-
-    def sing(self):
-		
+    def sing(self):		
         print(self.name, 'the', self.species, 'is singing!')
-
-
 
     def fly(self, distance):
         print(self.name, 'flew',distance, 'meters.')
 
-
-
     def current_age(self):
         self.age += 1
         print(self.name, 'You are now', self.age, 'years old.')
-
 
 
 # Create instances of the Bird class
@@ -31,11 +23,8 @@ class Bird:
 bird1 = Bird("Tweety", "Dove", 2)
 bird2 = Bird("Twinkle", "Parrot", 5)
 
-
 bird1.sing()
 bird1.fly(100)
-
-
 
 bird1.current_age()`
 
